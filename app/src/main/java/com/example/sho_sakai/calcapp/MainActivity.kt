5e3126f3d3a61c78c68ba0d31f511982d67cf031
Sho_SAKAI <sakaisho_1984@yahoo.co.jp>
2019-12-07 23:11:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sho_sakai/calcapp/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.sho_sakai.calcapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.util.Log
+import android.support.design.widget.Snackbar
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val num1 = editText1.text.toString().toDouble()
+        val num2 = editText2.text.toString().toDouble()
+        var test: Double = 0.0
+
+        //try {
+        //    test = num1 / num2
+        //} catch (e: Exception) {
+        //    Snackbar.make(v, "エラー", Snackbar.LENGTH_LONG)
+        //            .setAction("Action"){
+        //                Log.d("UI_PARTS", "Snackbarをタップした")
+        //            }.show()
+        //} finally {
+        //    Log.d("kotlintest", "ans = " + test.toString())
+        //}
+        if (num1 == null || num2 == null) {
+            Snackbar.make(v, "エラー", Snackbar.LENGTH_LONG)
+                       .setAction("Action"){
+                            Log.d("UI_PARTS", "Snackbarをタップした")
+                        }.show()
+            return}
+
+        else {
+            when (v.id) {
+                R.id.button1 -> {
+                    val plus = (editText1.text.toString().toDouble() + editText2.text.toString().toDouble())
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("VALUE1", plus)
+                    startActivity(intent)
+                }
+
+                R.id.button2 -> {
+                    val minus = (editText1.text.toString().toDouble() - editText2.text.toString().toDouble())
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("VALUE1", minus)
+                    startActivity(intent)
+                }
+
+                R.id.button3 -> {
+                    val multiply = (editText1.text.toString().toDouble() * editText2.text.toString().toDouble())
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("VALUE1", multiply)
+                    startActivity(intent)
+                }
+
+                R.id.button4 -> {
+                    val divide = (editText1.text.toString().toDouble() / editText2.text.toString().toDouble())
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("VALUE1", divide)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sho_sakai/calcapp/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sho_sakai.calcapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val value1 = intent.getDoubleExtra("VALUE1", 0.0)
+
+        textView.text = value1.toString()
+    }
+}
